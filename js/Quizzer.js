84f71bc9f78fa0b66c22aa0eaa5d0d34f827c406
MarkDuijf <markduijf@hotmail.nl>
2015-10-08 11:17:03 +0200

    PubQuiz setup screens

---
+++ js/Quizzer.js
@@ -0,0 +1,35 @@
+var theApp = angular.module("QuizzerApp", ['ngRoute']);
+
+
+theApp.config(['$routeProvider',
+    function($routeProvider) {
+        console.log("Hello! :)")
+        $routeProvider.
+            when('/participant', {
+                templateUrl: 'partials/participantsView.html'
+                //controller: 'RouteController'
+            }).
+            when('/hoster', {
+                templateUrl: 'partials/hosterView.html'
+            }).
+            when('/spectator', {
+                templateUrl: 'partials/beamerView.html',
+                controller: 'beamerViewController'
+            }).
+            when('/spectator:room?', {
+                templateUrl: 'partials/beamerView.html',
+                //controller: 'newMovieController'
+            }).
+            otherwise({
+                redirectTo: 'index'
+            });
+    }]);
+
+theApp.controller("beamerViewController", function($scope, $location){
+
+    $scope.openSpecRoom = function(){
+        $location.path("/beamerView/" + roomName);
+    };
+
+
+});
