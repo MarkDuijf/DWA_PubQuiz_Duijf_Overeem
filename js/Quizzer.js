9dcfea8fcb17ba03b1fb31b46f4742da71849847
MarkDuijf <markduijf@hotmail.nl>
2015-10-09 11:58:00 +0200

    PubQuiz seleced menu

--- js/Quizzer.js
+++ js/Quizzer.js
@@ -35,6 +35,56 @@ theApp.controller("beamerViewController", function($scope, $location){
         $location.path("/beamerView/" + roomName);
     };
 
-
-
 });
+
+
+
+
+theApp.controller('menuControl', ['$scope', '$location', function ($scope, $location) {
+
+    $scope.menuItems = [{
+        Title: 'Quizzer',
+        LinkText: 'home',
+        selected: true
+    }, {
+        Title: 'Participate',
+        LinkText: 'participant',
+        selected: false
+
+    }, {
+        Title: 'Host',
+        LinkText: 'hoster',
+        selected: false
+
+    }, {
+        Title: 'Spectate',
+        LinkText: 'spectator',
+        selected: false
+
+    }];
+
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };
+
+    $scope.toggleSelected = function(menu){
+        $scope.menuItems.forEach(function(menuitem){
+            if (menuitem === menu){
+                menu.selected = !menu.selected;
+                console.log(menu.selected);
+                return menu.selected;
+            }
+            else{
+                menuitem.selected = !menuitem.selected;
+                console.log(menuitem.selected);
+                return menu.selected;
+            }
+        });
+
+    }
+
+
+}]);
+
+
