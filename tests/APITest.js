01493a6d5a179978a17da2eccb20b16970e82fa1
MarkDuijf <markduijf@hotmail.nl>
2015-10-23 15:05:51 +0200

    Started API tests

---
+++ tests/APITest.js
@@ -0,0 +1,48 @@
+var expect = require('chai').expect;
+var supertest = require('supertest');
+var app = require('../server');
+var agent = supertest.agent(app);
+
+describe('Als een team een room wilt joinen', function(){
+    it('moet lege invoer weigeren', function(done) {
+        var login = {
+            teamName: '',
+            roomPass: '' ,
+            roomId: '1'};
+        agent
+            .post('/participant/joinRoom')
+            .send(login)
+            .set('Content-Type', 'application/json')
+            .expect(200)
+            .expect('Content-Type', /text\/html/)
+            .end(function(err,res) {
+                expect(err).to.be.null;
+
+                console.log(res.text);
+                expect(res.text).to.equal('the password was incorrect!');
+                done();
+            });
+    });
+    it('moet een gekozen teamnaam weigeren', function(done) {
+        var login = {
+            teamName: 'hallo',
+            roomPass: '1' ,
+            roomId: '1'};
+        agent
+            .post('/participant/joinRoom')
+            .send(login)
+            .set('Content-Type', 'application/json')
+            .expect(404, done)
+            .expect('Content-Type', /json/)
+            .end(function(err,res) {
+              expect(res.body).to.have.property('message');
+              done();
+            });
+    });
+    //xit('should refuse partial submissions', function() {
+    //
+    //});
+    //xit('should refuse complete submission of invalid user credentials', function() {
+    //
+    //});
+});
