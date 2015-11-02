2ee3358ed64ed39ca7a32852dc0281fef81aeae7
MarkDuijf <markduijf@hotmail.nl>
2015-11-02 10:39:39 +0100

    Test uitgebereid

--- tests/APITest.js
+++ tests/APITest.js
@@ -4,41 +4,49 @@ var app = require('../server');
 var agent = supertest.agent(app);
 
 describe('Als een team een room wilt joinen', function(){
-    it('moet lege invoer weigeren', function(done) {
-        var login = {
-            teamName: '',
-            roomPass: '' ,
-            roomId: '1'};
-        agent
-            .post('/participant/joinRoom')
-            .send(login)
-            .set('Content-Type', 'application/json')
-            .expect(200)
-            .expect('Content-Type', /text\/html/)
-            .end(function(err,res) {
-                expect(err).to.be.null;
+    Room.create({_id: '1', password: '123', teams: [{teamName: 'hallo', score: 0}], adminPass: '123', roundNr: 0, questionNr: 0}, function(err){
+    expect(err).to.be.null;
 
-                console.log(res.text);
-                expect(res.text).to.equal('the password was incorrect!');
-                done();
-            });
-    });
-    it('moet een gekozen teamnaam weigeren', function(done) {
-        var login = {
-            teamName: 'hallo',
-            roomPass: '1' ,
-            roomId: '1'};
-        agent
-            .post('/participant/joinRoom')
-            .send(login)
-            .set('Content-Type', 'application/json')
-            .expect(404, done)
-            .expect('Content-Type', /json/)
-            .end(function(err,res) {
-              expect(res.body).to.have.property('message');
-              done();
-            });
-    });
+        it('verkeerd wachtwoord weigeren', function(done) {
+            var login = {
+                teamName: '',
+                roomPass: '' ,
+                roomId: '1'};
+            agent
+                .post('/participant/joinRoom')
+                .send(login)
+                .set('Content-Type', 'application/json')
+                .expect(200)
+                .expect('Content-Type', /text\/html/)
+                .end(function(err,res) {
+                    expect(err).to.be.null;
+
+                    console.log(res.text);
+                    expect(res.text).to.equal('the password was incorrect!');
+                    done();
+                });
+        });
+
+        it('moet een gekozen teamnaam weigeren', function(done) {
+            var login = {
+                teamName: "hallo",
+                roomPass: '1' ,
+                roomId: '1'};
+            agent
+                .post('/participant/joinRoom')
+                .send(login)
+                .set('Content-Type', 'application/json')
+                .expect(404, done)
+                .expect('Content-Type', /json/)
+                .end(function(err,res) {
+                    expect(res.body).to.have.property('message');
+                    done();
+                });
+        });
+});
+
+
+
     //xit('should refuse partial submissions', function() {
     //
     //});
